cbfb2f5f1e2523299b6c3b3a746db2f9721270be
Chamon562 <Chamon562@gmail.com>
2020-07-23 14:45:43 -0700

    [rappers and dota hero] Add rapper and dota character

--- app.js
+++ app.js
@@ -65,3 +65,65 @@ mumbleRapperTwo.name = 'tekeshi69'
 
 console.log(mumbleRapperTwo.name)
 console.log(mumbleRapper.name)
+
+// constructer function
+//what is a function 
+//function is some type of action
+//fucntion did something and return some type of value
+//will be like function template with info in it and we gon put info it
+//constructor function user has to be capitlize
+//set whatever you want to pass in ins
+//shows three dots
+function User(name){
+    //an object in particuler use this in side the object and reffering to object
+    //this is refering to user
+    this.name = name
+    //setting this to this user
+    //return this and this is the user 
+    //constructor function will automatically return this
+    //so you dont have to return this
+    // but regular need some type of return value
+}
+
+//whenever use this constrcutor function  this is like our template
+// set variable use the tempelate and set in ADAM
+// key word New initate constructer function new
+let adam = new User('Adam')
+//now can console.log adam
+//any time we have a functio we need to return someting
+//this wil return something but ac
+let pete = new User('Pete')
+console.log(adam.name)
+console.log(pete.name)
+
+function NBAPlayer(name, team, threePointShooter){
+    //under the hood this = () this is equaling the object
+    this.name = name
+    this.team = team
+    this.threePointShooter = threePointShooter
+    //going to return all this for you automatically
+}
+
+let steph = new NBAPlayer('steph curry', 'warriors', true)
+console.log(steph)
+console.log(steph.name)
+
+function BESTRapper(name, label, goodRapper ){
+    this.name = name
+    this.label = label
+    this.goodRapper = goodRapper
+}
+
+let tupacShakur = new BESTRapper('Tupac Amaru Shakur', 'Death Row', true)
+
+console.log(tupacShakur)
+console.log(tupacShakur.name)
+
+function DOTAHero(name, role, type){
+    this.name = name
+    this.role = role
+    this.type = type
+}
+
+let Axe = new DOTAHero('Axe', 'Tank', 'Physical Damage')
+console.log(Axe)
