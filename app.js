5fb5a3422a4da960ec6ca4e6afbb8339be03c040
Chamon562 <Chamon562@gmail.com>
2020-07-23 15:56:36 -0700

    [class] Add github profile

--- app.js
+++ app.js
@@ -112,6 +112,9 @@ function BESTRapper(name, label, goodRapper ){
     this.name = name
     this.label = label
     this.goodRapper = goodRapper
+    this.intro = function(){
+        console.log('Greatest Rapper Ever is ' + this.name)
+    }
 }
 
 let tupacShakur = new BESTRapper('Tupac Amaru Shakur', 'Death Row', true)
@@ -120,13 +123,106 @@ console.log(tupacShakur)
 console.log(tupacShakur.name)
 console.log(nipsey)
 
-function DOTAHero(name, role, type){
+function DOTA2Hero(name, role, type){
     this.name = name
     this.role = role
     this.type = type
+    this.intro = function(){
+        console.log('Hi, my name is ' + this.name)
+    }
 }
-
-let axe = new DOTAHero('Axe', 'Tank', 'Strength')
-let invoker = new DOTAHero('Invoker', 'Disabler', 'Intellect')
+//console location are key words so can mess up if you put int he perameters 
+let axe = new DOTA2Hero('Axe', 'Tank', 'Strength')
+let invoker = new DOTA2Hero('Invoker', 'Disabler', 'Intellect')
 console.log(axe)
 console.log(invoker)
+//what do you do wit a function call it or invoke with parenthsis
+//not put in consolelog because when i run this functio it will do a console.log
+axe.intro()
+invoker.intro()
+nipsey.intro()
+tupacShakur.intro()
+//up here intro is our function were using
+//constructor functions is a template 
+
+class Car {
+    //here is where setup your constructor what you pass in to your class
+    //like a function
+    //constrcutor part info we put in
+    constructor(year, make, model, color){
+        this.year = year
+        this.make = make
+        this.model = model
+        this.color = color
+        //if we want to make methods
+    }
+    //drive is seperate function outside of that
+    //make method drive invoke
+    drive(){
+        console.log('Vroom')
+    }
+    intro(){
+        console.log('This car is a ' + this.make + ' ' + this.model)
+    }
+
+}
+
+let tesla = new Car('2020', 'Tesla', 'Model S', 'red')
+console.log(tesla)
+tesla.drive()
+tesla.intro()
+
+//here make a fetch call and grab our actual github profile 
+//https://api.github.com/users/romebell
+//makking fetch call and paass in url as string
+//grabs my github
+class GithubProfile {
+    constructor(username, name, url){
+        this.username = username
+        this.name = name
+        this.url = url
+    }
+    intro(){
+        //back ticks `` is usefull this can include java script code
+        console.log(`My name is ${this.name} and my user name is @${this.username}`)
+}
+
+}
+
+fetch('https://api.github.com/users/chamon562')
+.then(response =>{
+    return response.json()
+})
+.then(data =>{
+    // console.log(data)
+    let githubURL = data.url
+    // console.log(githubURL)
+    let githubUserName = data.login
+    // console.log(githubUserName)
+    let githubName = data.name
+    // console.log(githubName)
+    //set class
+    let channee = new GithubProfile(githubUserName, githubName, githubURL)
+    console.log(channee)
+
+    channee.intro()
+
+})
+
+//take items and make my own class
+//grab login name and url
+
+
+//now info and make new class
+
+fetch('https://api.github.com/users/Lizzwest')
+.then(response =>{
+    return response.json()
+})
+
+.then(data =>{
+    console.log(data)
+    const lizz = new GithubProfile(data.login, data.login, data.url)
+    console.log(lizz)
+    lizz.intro()
+})
